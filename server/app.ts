36f8e3ede38327a59920223d8c5401bf93592e58
rizkiromadoni <coradex06@gmail.com>
2024-07-21 22:45:11 +0700

    feat: error handling

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,24 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono()
 
 app.get("/", (c) => {
     return c.text("Hello, World!")
+})
+
+app.onError((error, c) => {
+    if (error instanceof HTTPException) {
+        return c.json({
+            status: "fail",
+            message: error.message
+        }, error.status)
+    }
+
+    return c.json({
+        status: "fail",
+        message: "Internal Server Error"
+    }, 500)
 })
 
 export default app
---
+++ server/lib/error.ts
@@ -0,0 +1,22 @@
+import { HTTPException } from "hono/http-exception";
+
+export class InvariantError extends HTTPException {
+    constructor(message: string) {
+        super(400, { message })
+    }
+}
+export class AuthenticationError extends HTTPException {
+    constructor(message: string) {
+        super(401, { message })
+    }
+}
+export class Authorization extends HTTPException {
+    constructor(message: string) {
+        super(403, { message })
+    }
+}
+export class NotFoundError extends HTTPException {
+    constructor(message: string) {
+        super(404, { message })
+    }
+}
