d2a249cb974fe4d576bd0c56f670cfe87a2b8c42
rizkiromadoni <coradex06@gmail.com>
2024-07-22 06:06:53 +0700

    feat: change database engine to postgres

--- server/routes/users/index.ts
+++ server/routes/users/index.ts
@@ -4,6 +4,7 @@ import { zValidator } from "@hono/zod-validator"
 import UserSchema from "./schema"
 import { InvariantError } from "../../lib/error"
 import { createUser, getUser } from "../../models/userModel"
+import prisma from "../../lib/prisma"
 
 const users = new Hono()
 
@@ -14,8 +15,13 @@ const users = new Hono()
 
         const isExist = await getUser({ username: payload.username, email: payload.email })
         if (isExist) throw new InvariantError("User already exist")
+
+        const userCount = await prisma.user.count({ where: { role: "SUPERADMIN" }})
         
-        const user = await createUser(payload)
+        const user = await createUser({
+            ...payload,
+            role: userCount === 0 ? "SUPERADMIN" : "USER"
+        })
         return c.json({
             status: "success",
             data: {
