f776ae8a97531049b5ccb9af46c9e671b053f173
rizkiromadoni <coradex06@gmail.com>
2024-08-06 11:41:46 +0700

    feat: genres database schema

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -12,3 +12,11 @@ export const users = pgTable("users", {
     createdAt: timestamp("created_at", { precision: 3 }).notNull().defaultNow(),
     updatedAt: timestamp("updated_at", { precision: 3 }).notNull().defaultNow().$onUpdate(() => new Date()),
 })
+
+export const genres = pgTable("genres", {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    slug: varchar("slug", { length: 255 }).notNull().unique(),
+    createdAt: timestamp("created_at", { precision: 3 }).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", { precision: 3 }).notNull().defaultNow().$onUpdate(() => new Date()),
+})
