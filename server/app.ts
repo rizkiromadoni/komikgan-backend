0da2844a06ca764c2b7cb30c88bfd6f8effef52e
rizkiromadoni <coradex06@gmail.com>
2024-08-20 01:21:04 +0700

    feat: environment

--- server/app.ts
+++ server/app.ts
@@ -16,7 +16,7 @@ const app = new OpenAPIHono()
 
 app.use("*", logger())
 app.use("*", cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: process.env.CORS_ORIGIN!,
   credentials: true
 }))
 
--- server/lib/redis.ts
+++ server/lib/redis.ts
@@ -1,4 +1,7 @@
 import Redis from "ioredis"
 
-const redis = new Redis()
+const redis = new Redis({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT!)
+})
 export default redis
